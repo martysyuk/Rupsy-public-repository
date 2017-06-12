40556b76f454492f9f5df4cba42e54fb02bff62a
martysyuk <martysyuk@gmail.com>
2017-06-12 14:35:37 +0700

    Обновление VKontakte Auto Reposter - Добавлена автоматическая авторизация - Изменен способ хранения настроек программы

--- vk-auto-reposter/vkar.py
+++ vk-auto-reposter/vkar.py
@@ -10,29 +10,36 @@ Skype: martysyuk
 2) сделать автоматическую авторизацию уазанного пользователя
 """
 
-import vkapi
+import vk_api
 import time
 import pandas as pd
-import config as cfg
+import json
 
 
-class Wrapper:
+class Main:
 
-    def __init__(self):
+    def __init__(self, _file_name):
+        print('Starting searching posts...')
+
+        try:
+            with open(_file_name, 'r', encoding='UTF8') as cfg_file:
+                self.cfg = json.load(cfg_file)
+        except FileNotFoundError:
+            exit('Configuration file not found! Program exit...')
+
+        self.vk = vk_api.VKAuth(self.cfg['api']['scope'],
+                                self.cfg['api']['app_id'],
+                                self.cfg['api']['api_ver'],
+                                self.cfg['api']['login'],
+                                self.cfg['api']['password'])
+        self.vk.auth()
 
         self.df = pd.DataFrame(columns=['owner_id', 'post_id', 'likes'])
 
-        self.interests = cfg.interests
-
-        self.vk_request = vkapi.VKApiRequest(cfg.app_id, cfg.token_url)
-        self.users_id = self.vk_request.get_user_id(cfg.user_id)
-
+        self.interests = self.cfg['search']['interests']
         self.groups_with_interests = dict()
-
         self.now_time = time.time()
-        # 60 seconds * 60 minutes * 24 hours in day.
         self.maximum_old_posts_time = self.now_time - 60 * 60 * 24
-
         self.post_to_repost = list()
         self.already_posted = list()
 
@@ -40,35 +47,35 @@ class Wrapper:
         query = {'type': 'group',
                  'country_id': 1,
                  'sort': 2,
-                 'count': cfg.maximum_checking_count}
+                 'count': self.cfg['search']['maximum_checking_count']}
         groups_list = []
         for interest in self.interests:
             query.update({'q': interest})
-            response = self.vk_request.response('groups.search', query)
+            response = self.vk.get_response('groups.search', query)
             for each in response['items']:
                 groups_list.append('-'+str(each['id']))
             self.groups_with_interests.update({interest: groups_list})
             groups_list = []
 
     def search_posts_with_max_likes(self):
-        query = {'count': cfg.maximum_checking_count,
+        query = {'count': self.cfg['search']['maximum_checking_count'],
                  'filter': 'owner'}
         for each in self.groups_with_interests:
             for group_id in self.groups_with_interests[each]:
                 query.update({'owner_id': group_id})
-                _response = self.vk_request.response('wall.get', query)
+                _response = self.vk.get_response('wall.get', query)
                 try:
                     for post in _response['items']:
                         if post['date'] > self.maximum_old_posts_time:
                             self.df.loc[len(self.df)] = [post['owner_id'], post['id'], post['likes']['count']]
                 except TypeError:
                     pass
-            self.df = self.df.sort_values('likes', ascending=0).head(cfg.reposts_count)
+            self.df = self.df.sort_values('likes', ascending=0).head(self.cfg['repost']['reposts_count'])
             self.repost()
             self.df = pd.DataFrame(columns=['owner_id', 'post_id', 'likes'])
-            if cfg.repost_wait > 0:
-                print('Waiting {} seconds to next repost...'.format(cfg.repost_wait))
-                time.sleep(cfg.repost_wait)
+            if self.cfg['repost']['repost_wait'] > 0:
+                print('Waiting {} seconds to next repost...'.format(self.cfg['repost']['repost_wait']))
+                time.sleep(self.cfg['repost']['repost_wait'])
 
     def repost(self):
         for each in range(len(self.df)):
@@ -77,18 +84,14 @@ class Wrapper:
                                                           str(_getter['post_id'])).replace('.0', ''))
         for each in self.post_to_repost:
             if each not in self.already_posted:
-                self.vk_request.response('wall.repost', {'object': each,
-                                                         'group_id': cfg.repost_to,
-                                                         'message': cfg.message})
+                self.vk.get_response('wall.repost', {'object': each,
+                                                     'group_id': self.cfg['repost']['repost_to'],
+                                                     'message': self.cfg['repost']['add_tags']})
                 self.already_posted.append(each)
                 print('Repost done!')
 
 
-def main():
-    print('Starting searching posts...')
-    wrapper = Wrapper()
+if __name__ == '__main__':
+    wrapper = Main('vkar.json')
     wrapper.get_groups_by_interests()
     wrapper.search_posts_with_max_likes()
-
-if __name__ == '__main__':
-    main()
