0ef41c971782487f292c91e445abdf1acf22e48a
RuPsy <martysyuk@gmail.com>
2018-01-24 12:14:30 +0700

    Function checking point falls in a polygon
    Добавил пример функции проверяющий попадает ли точка в границы многоугольника.

---
+++ inPoligon.py
@@ -0,0 +1,17 @@
+def point_in_polygon(pnt, plgn):
+    x = pnt[0]
+    y = pnt[1]
+    c = 0
+    xp = []
+    yp = []
+    for ppnt in plgn:
+        xp.append(ppnt[0])
+        yp.append(ppnt[1])
+    for i in range(len(xp)):
+        if (((yp[i] <= y < yp[i - 1]) or (yp[i - 1] <= y < yp[i])) and
+                (x > (xp[i - 1] - xp[i]) * (y - yp[i]) / (yp[i - 1] - yp[i]) + xp[i])):
+            c = 1 - c
+    return True if c == 1 else False
+
+polygon = [(0, 0), (100, 0), (100, 100), (50, 50), (0, 50)]
+print(point_in_polygon((51, 50), polygon))
